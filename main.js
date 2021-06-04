778de23706ad5c95bcc1176375a2e882aef8fcd2
ImDiplan <diplan0120@gmail.com>
2021-06-03 22:59:37 -0400

    tiene un error en el js

---
+++ main.js
@@ -0,0 +1,14 @@
+function GuardarDatos(){
+//     let Datos = document.getElementById('input').value;
+//     document.getElementById('name').innerHTML=Datos;
+// id();
+    for(let i=0;i<20; i++){
+      const tabla = document.getElementgetElementById('tabla');
+      tabla.innerHTML('<p>Hello World!</p>');
+    }
+};
+let i = 0;
+const id = ()=> {
+    i++;
+    document.getElementById('id').innerHTML=i;
+}
