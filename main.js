aef260fc86c2cc6664be02da246196e49eaa2e53
ImDiplan <diplan0120@gmail.com>
2021-06-29 00:13:54 -0400

    añadí nuevos estilos y agregué otro botón

--- main.js
+++ main.js
@@ -1,40 +1,54 @@
-function GuardarDatos(){
-    
-    let Datos= document.getElementById('input').value;
+function GuardarDatos() {
+
+    let Datos = document.getElementById('input').value;
     id();
-    document.getElementById(`name${i}`).innerHTML=Datos;
-    
+    document.getElementById(`name${i}`).innerHTML = Datos;
+
 };
 let i = 0;
 const id = () => {
-  i++;
-  agregarFila();
-    document.getElementById(`id${i}`).innerHTML=i;
-    
+    i++;
+    agregarFila();
+    document.getElementById(`id${i}`).innerHTML = i;
+
 
 };
 
 
-function agregarFila(){
+
+function agregarFila() {
     const tbody = document.getElementById('tabla');
     const tcol = document.createElement('tr');
     const tfil = document.createElement('td');
     const tfiln = document.createElement('td');
     const tfilb = document.createElement('td');
-    const btnBorrar = document.createElement('a');
+    const btnEditar = document.createElement('btn');
+    const txtEditar = document.createElement('i');
+    const btnBorrar = document.createElement('btn');
+    const txtBorrar = document.createElement('i');
+    btnEditar.classList.add('btn', 'btn-success');
+    txtEditar.classList.add('fas', 'fa-edit');
     btnBorrar.classList.add('btn', 'btn-danger');
-    btnBorrar.textContent = "Borrar";
-    btnBorrar.id=`btn${i}`;
+    txtBorrar.classList.add('far', 'fa-trash-alt');
+    btnEditar.appendChild(txtEditar);
+    btnBorrar.appendChild(txtBorrar);
+    tfilb.appendChild(btnEditar);
     tfilb.appendChild(btnBorrar);
-    tfiln.id=`name${i}`;
+    tfil.classList.add('text-center');
+    tfilb.classList.add('text-center');
+    tfiln.classList.add('text-center');
+    tfiln.id = `name${i}`;
     tcol.appendChild(tfil);
-    tfil.id=`id${i}`;
+    tfil.id = `id${i}`;
     tcol.appendChild(tfiln);
     tcol.appendChild(tfilb);
-    tcol.id=`col${i}`;
+    tcol.id = `col${i}`;
     tbody.appendChild(tcol);
-    btnBorrar.addEventListener("click", () =>{
+    btnBorrar.addEventListener("click", () => {
         const col = btnBorrar.parentElement;
         col.parentElement.remove();
-     })
+    });
+    btnEditar.addEventListener("click", () => {
+        
+    })
 }
